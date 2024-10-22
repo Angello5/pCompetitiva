caf0af9f893a22de3bb6d61744ea0421f5650a4f
Angello Llerena <angello.llerena@usil.pe>
2024-10-22 10:32:20 -0500

    semana10

---
+++ Semana10/fb_dinamica.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int fib(int n, vector<int> &memo){
+    if(n <= 1){
+        return n;
+    }
+    if(memo[n] != -1){
+        return memo[n];
+    }
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    
+    return memo[n];
+}
+
+int main(){
+    int n = 5;
+    vector<int> memo(n + 1, -1); 
+
+    cout << fib(n, memo);
+    return 0;   
+}
